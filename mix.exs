242352e38f0ff94cdb81d26b19e953bd41379db7
mononym <1351942+mononym@users.noreply.github.com>
2023-04-25 00:56:32 -0700

    update ex_doc to 0.29

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule GitDiff.Mixfile do
     [
       {:benchee, "~> 1.0", only: :dev},
       {:excoveralls, "~> 0.12.0", only: :test},
-      {:ex_doc, "~> 0.21.2", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
 end
